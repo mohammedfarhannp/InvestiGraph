618aff087df62af76d530b7725612bb58f5940ea
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-13 19:04:32 +0530

    image loading code added!

--- entities/person.py
+++ entities/person.py
@@ -5,8 +5,11 @@ from settings import COLOR_PERSON_MALE, COLOR_PERSON_FEMALE
 class Person(Node):
     def __init__(self, node_id, label, x, y, gender="male"):
         color = COLOR_PERSON_MALE if gender == "male" else COLOR_PERSON_FEMALE
-        super().__init__(node_id, "Person", label, x, y, color)
+        
         self.gender = gender
+        icon_path = f"assets/icons/{gender}.png"
+        
+        super().__init__(node_id, "Person", label, x, y, color, icon_path)
         self.properties = {
             "name": label,
             "gender": gender,
