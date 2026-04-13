4a639f9c12f2a99376b87ba65002263b19a3ac7a
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-13 19:16:00 +0530

    Entity Email!

---
+++ entities/email.py
@@ -0,0 +1,17 @@
+# entities/email.py
+from core.node import Node
+from settings import COLOR_EMAIL
+
+class Email(Node):
+    def __init__(self, node_id, label, x, y):
+        icon_path = "assets/icons/email.png"
+        super().__init__(node_id, "Email", label, x, y, COLOR_EMAIL, icon_path)
+        self.properties = {
+            "address": label,
+            "associated_with": []  # List of person/organization IDs
+        }
+    
+    def to_dict(self):
+        data = super().to_dict()
+        data["properties"] = self.properties
+        return data
