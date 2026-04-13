2d31ee746cbaaddb1e2db96ea3659cf7864a8dd3
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-13 19:36:19 +0530

    Entity Phone for Phone Number Node!

---
+++ entities/phone.py
@@ -0,0 +1,17 @@
+# entities/phone.py
+from core.node import Node
+from settings import COLOR_PHONE
+
+class Phone(Node):
+    def __init__(self, node_id, label, x, y):
+        icon_path = "assets/icons/phone.png"
+        super().__init__(node_id, "Phone", label, x, y, COLOR_PHONE, icon_path)
+        self.properties = {
+            "number": label,
+            "associated_with": []  # List of person/organization IDs
+        }
+    
+    def to_dict(self):
+        data = super().to_dict()
+        data["properties"] = self.properties
+        return data
