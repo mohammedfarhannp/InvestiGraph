1fd35cfc8ce6b01df44c07353682cc1e9a0d1d2c
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-13 05:57:36 +0530

    Constants!

--- settings.py
+++ settings.py
@@ -11,3 +11,16 @@ BACKGROUND_COLOR = (30, 30, 35)  # Dark gray
 APPLICATION_TITLE = "InvestiGraph"
 
 FPS = 60
+
+# Node Colors
+COLOR_PERSON_MALE = (100, 150, 255)    # Light Blue
+COLOR_PERSON_FEMALE = (255, 150, 200)  # Light Pink
+COLOR_ORGANIZATION = (255, 165, 0)     # Orange
+COLOR_EMAIL = (100, 255, 100)          # Light Green
+COLOR_PHONE = (180, 100, 255)          # Purple
+COLOR_DOCUMENT = (255, 255, 100)       # Yellow
+COLOR_DATABASE = (128, 128, 128)       # Gray
+
+# Node Defaults
+DEFAULT_NODE_RADIUS = 30
+DEFAULT_FONT_SIZE = 12
