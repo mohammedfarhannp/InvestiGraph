ce8e51a5772ff56b310a8345f84a2ffd95d27a52
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-17 00:35:03 +0530

    ICON path optimization!

--- entities/organization.py
+++ entities/organization.py
@@ -1,10 +1,10 @@
 # entities/organization.py
 from core.node import Node
-from settings import COLOR_ORGANIZATION
+from settings import COLOR_ORGANIZATION, ORGANIZATION_ICON
 
 class Organization(Node):
     def __init__(self, node_id, label, x, y):
-        icon_path = "assets/icons/organization.png"
+        icon_path = ORGANIZATION_ICON
         super().__init__(node_id, "Organization", label, x, y, COLOR_ORGANIZATION, icon_path)
         self.properties = {
             "name": label,
