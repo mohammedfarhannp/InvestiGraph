8c48fdb2a76d62e7937c72ada944e75709933505
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-17 00:34:07 +0530

    ICON path optimization!

--- entities/social_media.py
+++ entities/social_media.py
@@ -1,10 +1,10 @@
 # entities/social_media.py
 from core.node import Node
-from settings import COLOR_SOCIAL_MEDIA
+from settings import COLOR_SOCIAL_MEDIA, SOCIAL_MEDIA_ICON
 
 class SocialMedia(Node):
     def __init__(self, node_id, label, x, y, platform=None):
-        icon_path = "assets/icons/social_media.png"
+        icon_path = SOCIAL_MEDIA_ICON
         super().__init__(node_id, "SocialMedia", label, x, y, COLOR_SOCIAL_MEDIA, icon_path)
         self.platform = platform  # Twitter, Facebook, LinkedIn, etc.
         self.properties = {
