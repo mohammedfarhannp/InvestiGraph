1eb9eb963153c241dd84bed2caa0b46927bf8176
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-17 00:34:37 +0530

    ICON path optimization!

--- entities/document.py
+++ entities/document.py
@@ -1,10 +1,10 @@
 # entities/document.py
 from core.node import Node
-from settings import COLOR_DOCUMENT
+from settings import COLOR_DOCUMENT, DOCUMENT_ICON
 
 class Document(Node):
     def __init__(self, node_id, label, x, y):
-        icon_path = "assets/icons/document.png"
+        icon_path = DOCUMENT_ICON
         super().__init__(node_id, "Document", label, x, y, COLOR_DOCUMENT, icon_path)
         self.properties = {
             "title": label,
