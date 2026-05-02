0dd7950abf0909b3515c8fdb01436636fb1a6f4c
mohammedfarhannp <m.farhan.np@gmail.com>
2026-05-02 22:50:36 +0530

    Simple Window Runs for now!
    Needs to recreate entire investigraph here.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+// src/main.rs
+use macroquad::prelude::*;
+
+#[macroquad::main("InvestiGraph")]
+async fn main() {
+    loop {
+
+        clear_background(Color::new(30.0/255.0, 30.0/255.0, 35.0/255.0, 1.0));
+
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
+
+        next_frame().await;
+    }
+
 }
