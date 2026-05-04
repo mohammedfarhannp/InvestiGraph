a17eed6220a25ab3a4147ee871ab45ee6d5bd88a
mohammedfarhannp <m.farhan.np@gmail.com>
2026-05-04 21:26:19 +0530

    Works! Panning and Zooming!

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -15,7 +15,8 @@ async fn main() {
     // Camera Variables Set!
     let mut camera = Camera::new();
     let mut camera_dragging : bool = false;
-    let drag_start = (0.0, 0.0);
+    let mut drag_start = (0.0, 0.0);
+    let mut drag_start_camera = (0.0, 0.0);
 
     // Main Loop (I guess)
     loop {
@@ -23,7 +24,7 @@ async fn main() {
 
         // Draw Grid
         let start_x = (-camera.x / camera.zoom / GRID_SPACING) as i32 -1;
-        let start_y = (-camera.x / camera.zoom / GRID_SPACING) as i32 -1;
+        let start_y = (-camera.y / camera.zoom / GRID_SPACING) as i32 -1;
 
         let end_x = start_x + (SCREEN_WIDTH / camera.zoom / GRID_SPACING) as i32 + 2;
         let end_y = start_y + (SCREEN_HEIGHT / camera.zoom / GRID_SPACING) as i32 + 2;
@@ -34,26 +35,28 @@ async fn main() {
                 let screen_y = j as f32 * GRID_SPACING * camera.zoom + camera.y;
 
 
-                if screen_x >= -GRID_SPACING && screen_x <= SCREEN_WIDTH + GRID_SPACING && screen_y >= RIBBON_HEIGHT && screen_y <= SCREEN_HEIGHT + GRID_SPACING {
+                if screen_x >= -GRID_SPACING && screen_x <= SCREEN_WIDTH + GRID_SPACING && screen_y >= RIBBON_HEIGHT {
                     draw_circle(screen_x, screen_y, 2.0, rgb(GRID_COLOR));
                 }
             }
         }
 
         // Camera Controls
-        if is_mouse_button_down(MouseButton::Left) && !camera_dragging {
-            camera_dragging = true;
-        }
-
-        // Camera Pan Logic
-        if camera_dragging {
-            if is_mouse_button_down(MouseButton::Left) {
-                let (mouse_x, mouse_y) = mouse_position();
+        if is_mouse_button_down(MouseButton::Left) {
+            if !camera_dragging {
+                camera_dragging = true;
+                drag_start = mouse_position();
+                drag_start_camera = (camera.x, camera.y);
             } else {
-                camera_dragging = false;
+                let (mouse_x, mouse_y) = mouse_position();
+                camera.x = drag_start_camera.0 + (mouse_x - drag_start.0);
+                camera.y = drag_start_camera.1 + (mouse_y - drag_start.1);
             }
+        } else {
+            camera_dragging = false;
         }
 
+    
         // Zoom with mouse wheel
         let wheel = mouse_wheel();
         if wheel.1 != 0.0 {
